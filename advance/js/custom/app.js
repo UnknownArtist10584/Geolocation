6be982215a6d93427d075d22c283b5a76e19e765
UnknownArtist10584 <varungoofygumball@gmail.com>
2016-07-04 11:50:41 +0530

    Added error handling for browser rejecting location access

--- advance/js/custom/app.js
+++ advance/js/custom/app.js
@@ -2,72 +2,82 @@ var app = angular.module('Geolocation', []);
 
 app.controller('GeolocationController', function($scope) {
 
-    $scope.geocoder = new google.maps.Geocoder;
-    $scope.uaddress = '';
-    $scope.ulocation = '';
+  $scope.geocoder = new google.maps.Geocoder;
+  $scope.uaddress = '';
+  $scope.ulocation = '';
 
 
-    $scope.geoProcess = function() {
-        console.log("geoProcess processing");
+  $scope.geoProcess = function() {
+    console.log("geoProcess processing");
 
-        $scope.geoLatLng();
+    $scope.geoLatLng();
+  }
+
+  $scope.geoLatLng = function() {
+    console.log("geoLatLng processing");
+
+    if (navigator && navigator.geolocation) {
+      console.log("geolocation provider found");
+
+      navigator.geolocation.getCurrentPosition(function(location) {
+
+          console.log(location.coords);
+
+          $scope.ulocation = location.coords.latitude + ',' + location.coords
+            .longitude;
+          $scope.$apply();
+          $scope.geoAddress($scope.geocoder);
+        },
+        function(error) {
+          if (error.code === error.PERMISSION_DENIED) {
+            alert(
+              "Your browser denied permission to access your location. Please change this in your browser's settings."
+            )
+          }
+        }
+      );
+
+    } else {
+      alert("error: geolocation provider is missing");
     }
 
-    $scope.geoLatLng = function() {
-        console.log("geoLatLng processing");
+  };
 
-        if (navigator && navigator.geolocation) {
-            console.log("geolocation provider found");
 
-            navigator.geolocation.getCurrentPosition(function(location) {
+  $scope.geoAddress = function(geocoder) {
+    console.log("geoAddress processing");
 
-                console.log(location.coords);
+    var ulocationdata = $scope.ulocation;
+    console.log("Input string is: " + ulocationdata);
 
-                $scope.ulocation = location.coords.latitude + ',' + location.coords.longitude;
-                $scope.$apply();
-                $scope.geoAddress($scope.geocoder);
-            });
-
-        } else {
-            alert("error: geolocation provider iscmissing");
-        }
-
+    var latlngStr = ulocationdata.split(',', 2);
+    console.log("Input is split into: " + latlngStr[0] + " and " +
+      latlngStr[1]);
+    var latlng = {
+      lat: parseFloat(latlngStr[0]),
+      lng: parseFloat(latlngStr[1])
+        //lat: 12.9601508,
+        //lng: 77.7159555
     };
 
+    geocoder.geocode({
+      'location': latlng
+    }, function(results, status) {
+      if (status === google.maps.GeocoderStatus.OK) {
+        if (results[1]) {
+          console.log(results[1].formatted_address);
+          $scope.uaddress = "Your location is " + results[0].formatted_address;
+          $scope.$apply();
+          console.log("Successfully wrote the address to uaddress");
+        } else {
+          window.alert("No results found")
+        }
+      } else {
+        window.alert("Geocoder failed due to " + status)
+      }
+    });
 
-    $scope.geoAddress = function(geocoder) {
-        console.log("geoAddress processing");
-
-        var ulocationdata = $scope.ulocation;
-        console.log("Input string is: " + ulocationdata);
-
-        var latlngStr = ulocationdata.split(',', 2);
-        console.log("Input is split into: " + latlngStr[0] + " and " + latlngStr[1]);
-        var latlng = {
-            lat: parseFloat(latlngStr[0]),
-            lng: parseFloat(latlngStr[1])
-                //lat: 12.9601508,
-                //lng: 77.7159555
-        };
-
-        geocoder.geocode({
-            'location': latlng
-        }, function(results, status) {
-            if (status === google.maps.GeocoderStatus.OK) {
-                if (results[1]) {
-                    console.log(results[1].formatted_address);
-                    $scope.uaddress = "Your location is " + results[0].formatted_address;
-                    $scope.$apply();
-                    console.log("Successfully wrote the address to uaddress");
-                } else {
-                    window.alert("No results found")
-                }
-            } else {
-                window.alert("Geocoder failed due to " + status)
-            }
-        });
-
-    }
+  }
 
 
 });
